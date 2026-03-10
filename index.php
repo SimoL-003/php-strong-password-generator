d7d01a3a1a7119ac0ef6e65da58aa9adea2537e0
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-03-10 18:49:55 +0100

    refactor: improve password generation logic to include a wider character set

--- index.php
+++ index.php
@@ -12,12 +12,30 @@
 
 $password_length = $_GET['length'];
 
+// function generatePassword($password_length)
+// {
+//     $password = random_bytes($password_length);
+//     return bin2hex($password);
+// }
+// ;
+
 function generatePassword($password_length)
 {
-    $password = random_bytes($password_length);
-    return bin2hex($password);
+    $lower = 'abcdefghijklmnopqrstuvwxyz';
+    $upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numbers = '0123456789';
+    $special = '!@#$%^&*()-_=+[]{};:,.<>?';
+
+    $charachters = "$lower $upper $numbers $special";
+    $max_num = strlen($charachters) - 1;
+    $password = "";
+
+    for ($i = 0; $i < $password_length; $i++) {
+        $password .= $charachters[random_int(0, $max_num)];
+    }
+
+    return $password;
 }
-;
 
 
 ?>
